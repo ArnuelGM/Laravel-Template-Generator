985c4097bc5fa9123b18df922626cd85d874328e
Arnuel Gutierrez <argume31@hotmail.com>
2022-08-06 15:13:27 -0500

    ✨ feat: config jetstream for authentication features

---
+++ database/migrations/2022_08_06_150139_create_doc_placeholders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doc_placeholders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('document_id');
+            $table->string('label');
+            $table->string('placeholder');
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doc_placeholders');
+    }
+};
